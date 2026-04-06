1e255f2756a36a6ac9150a3d7efd82ab0940b3da
Aryan <costomf0004@gmail.com>
2026-04-06 21:55:58 +0530

    Added fps counter

--- skeleton/main.py
+++ skeleton/main.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-
+import time
 #only works for mediapipe 10.90
 mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
@@ -9,7 +9,8 @@ hands = mp_hands.Hands()
 pose = mp_pose.Pose()
 
 cap = cv2.VideoCapture(0)
-
+prev_time = 0
+curr_time = 0
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -24,8 +25,11 @@ while True:
             mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
     if pose_result.pose_landmarks:
             mp_draw.draw_landmarks(frame, pose_result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    curr_time = time.time()
+    fps = 1 / (curr_time - prev_time) if prev_time != 0 else 0
+    prev_time = curr_time
+    cv2.putText(frame, "FPS: " + str(int(fps)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow("InstrumentalCV", frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
