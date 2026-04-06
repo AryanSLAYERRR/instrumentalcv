784cffa9a145f01ab42e431d9749942507de718e
Aryan <costomf0004@gmail.com>
2026-04-06 21:49:51 +0530

    Added Mediapipe tracking with hand landmarks

--- skeleton/main.py
+++ skeleton/main.py
@@ -1,14 +1,29 @@
 import cv2
 import mediapipe as mp
 
+#only works for mediapipe 10.90
+mp_hands = mp.solutions.hands
+mp_pose = mp.solutions.pose
+mp_draw = mp.solutions.drawing_utils
+hands = mp_hands.Hands()
+pose = mp_pose.Pose()
+
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Error: Webcam not found/In use of other application")
+        print("Error: Webcam not found/Is in use of other application")
         break
     frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    hand_result = hands.process(rgb)
+    pose_result = pose.process(rgb)
+    if hand_result.multi_hand_landmarks:
+        for hand in hand_result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
+    if pose_result.pose_landmarks:
+            mp_draw.draw_landmarks(frame, pose_result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     cv2.imshow("InstrumentalCV", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
